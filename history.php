297b5b1fa8fa046d1de5f879434d4510df22419c
nrfkhira <93339223+nrfkhira@users.noreply.github.com>
2022-02-04 12:20:54 +0800

    Add files via upload

---
+++ history.php
@@ -0,0 +1,49 @@
+<?php include 'includes/config.php'; ?>
+<?php include 'includes/header.php'; ?>
+
+
+<section class="vh-100" style="background-color: #470ab1;">
+  <div class="container">
+    <div class="row w-100">
+        <div class="col-lg-12 col-md-12 col-12">
+        	<h1><center><i style="color: white; font-size: 1.5em;"></i><i style="color: white"> History </h1></center>
+			<table class="table table-dark table-striped">
+			  <thead>
+			    <tr>
+			      <th scope="col">Num</th>
+			      <th scope="col">Email</th>
+			      <th scope="col">Domain</th>
+			      <th scope="col">Timestamp</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <th scope="row">1</th>
+			      <td>php@gmail.com</td>
+			      <td>netbytesec.com</td>
+			      <td>22:00</td>
+			    </tr>
+			    <tr>
+			      <th scope="row">2</th>
+			      <td>test@gmail.com</td>
+			      <td>unisza.com</td>
+			      <td>23:00</td>
+			    </tr>
+			    <tr>
+			      <th scope="row">3</th>
+			      <td>php@gmail.com</td>
+			      <td>facebook.com</td>
+			      <td>14:00</td>
+			    </tr>
+			  </tbody>
+			</table>
+		</div>
+	</div>
+ </div>
+</section>
+
+
+ <?php include 'includes/footer.php'; ?>
+ 
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,58 @@
+
+
+    <!-- Footer Starts Here -->
+    <footer>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 col-sm-8 col-xs-18">
+            <div class="footer-item">
+              <div class="footer-heading">
+                <h2>About Us</h2>
+              </div>
+              <p>This tool works by generating a large list of permutations based on a domain name you provide and then checking if any of those permutations are in use and generate fuzzy hashes of the web pages to see if they are part of an ongoing phishing attack or brand impersonation, and much more</p>
+            </div>
+          </div>
+          
+          
+        
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="footer-item">
+              <div class="footer-heading">
+                <h2>Useful Links</h2>
+              </div>
+              <ul class="footer-list">
+                <li><a href="#">Cloud Hosting Platform</a></li>
+                <li><a href="#">Light Speed Zone</a></li>
+                <li><a href="#">Content Delivery Network</a></li>
+                <li><a href="#">Customer Support</a></li>
+                <li><a href="https://github.com/elceef/dnstwist">github.dnstwist</a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="footer-item">
+              <div class="footer-heading">
+                <h2>More Information</h2>
+              </div>
+              <ul class="footer-list">
+                <li>Phone: <a href="#">010-020-0560</a></li>
+                <li>Email: <a href="#">mail@company.com</a></li>
+                <li>Support: <a href="#">support@company.com</a></li>
+                <li>Website: <a href="#">www.company.com</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="sub-footer">
+              <p>Copyright &copy; 2020 Cloud Hosting Company
+                - Designed by <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer Ends Here -->
+
+<?php include 'script.php'; ?>
